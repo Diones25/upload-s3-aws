3ccc16f41598381eb93f89adf76bf1c192ff4b9f
Diones <pereiradiones987@gmail.com>
2025-10-03 15:25:00 -0300

    update UploadModule

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
-import { AwsS3Module } from 'src/aws-s3/aws-s3.module';
 
 @Module({
-  imports: [AwsS3Module],
   providers: [UploadService],
-  controllers: [UploadController]
+  controllers: [UploadController],
+  exports: [UploadService],
 })
 export class UploadModule {}
