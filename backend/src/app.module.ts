2bc0e91cf7dbb89e6ac83212eaf7e47962f111a8
Diones <pereiradiones987@gmail.com>
2025-10-03 10:17:45 -0300

    modulo client S3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AwsS3Module } from './aws-s3/aws-s3.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AwsS3Module,
   ],
   controllers: [],
   providers: [],
